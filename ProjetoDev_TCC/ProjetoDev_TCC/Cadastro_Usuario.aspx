279d682b6fe432adcf0f2583f0d57df967ce295b
jhonatan <williamjhonatanxd@gmail.com>
2022-12-01 20:47:20 -0300

    pagina cadastro

---
+++ ProjetoDev_TCC/ProjetoDev_TCC/Cadastro_Usuario.aspx
@@ -0,0 +1,3 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Cadastro_Usuario.aspx.cs" Inherits="ProjetoDev_TCC.Cadastro_Usuario" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+</asp:Content>
---
+++ ProjetoDev_TCC/ProjetoDev_TCC/Cadastro_Usuario.aspx.designer.cs
@@ -0,0 +1,15 @@
+﻿//------------------------------------------------------------------------------
+// <gerado automaticamente>
+//     Este código foi gerado por uma ferramenta.
+//
+//     As alterações ao arquivo poderão causar comportamento incorreto e serão perdidas se
+//     o código for recriado
+// </gerado automaticamente>
+//------------------------------------------------------------------------------
+
+namespace ProjetoDev_TCC {
+    
+    
+    public partial class Cadastro_Usuario {
+    }
+}
